012cf3c1f44e2bc847d841516184c09e66a83548
ramazanfirin <ramazan_firin@hotmail.com>
2022-07-02 15:33:56 -0700

    product düzenlendi

--- src/main/java/com/smartiq/pim/repository/ProductRepository.java
+++ src/main/java/com/smartiq/pim/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.smartiq.pim.repository;
 
 import com.smartiq.pim.domain.Product;
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +11,7 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p where p.category.id = :categoryId")
+    List<Product> getProductListOfCategory(@Param("categoryId") Long categoryId);
+}
